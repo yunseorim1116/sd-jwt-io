02173978d523387219b1fbd37ba8dc64deb8b391
tree <hhyjs2r@gmail.com>
2024-02-20 19:10:54 +0900

    style: create common button

---
+++ components/common/Button.tsx
@@ -0,0 +1,21 @@
+// CommonButton.tsx
+
+import React, { ReactNode } from "react";
+
+interface CommonButtonProps {
+  onClick: () => Promise<void>; // 수정 가능한 타입으로 변경
+  children: ReactNode;
+}
+
+const Button = ({ onClick, children }: CommonButtonProps) => {
+  return (
+    <button
+      className="bg-blue-500 text-white py-2 px-4 rounded-md transition duration-300 hover:bg-blue-600"
+      onClick={onClick}
+    >
+      {children}
+    </button>
+  );
+};
+
+export default Button;
