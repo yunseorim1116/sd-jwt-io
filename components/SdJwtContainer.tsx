49deef4b643adb082d6f36d7ae09bcdaaf026e61
tree <hhyjs2r@gmail.com>
2024-02-21 14:46:05 +0900

    feat: create dev branch

--- components/SdJwtContainer.tsx
+++ components/SdJwtContainer.tsx
@@ -2,6 +2,7 @@ import React, { ReactNode } from "react";
 import { Warning } from "./Warning";
 
 export const SdJwtContainer = ({ children }: { children: ReactNode }) => {
+  console.log("created dev branch");
   return (
     <main className="min-h-screen items-center p-24">
       <div className="flex justify-center items-center text-4xl m-6 mt-12">
